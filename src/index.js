99d786ff1b662ed098b93f15d7250fbf85a04183
Himanshu Bhatt <hbhatt034@gmail.com>
2024-01-30 06:30:17 +0530

    Added Classes

--- src/index.js
+++ src/index.js
@@ -11,6 +11,27 @@ function addTodo(todoData) {
   let finishedButton = document.createElement("button");
   let hr = document.createElement("hr");
 
+  rowDiv.classList.add("row");
+  todoItem.classList.add(
+    "todo-item",
+    "d-flex",
+    "flex-row",
+    "justify-content-between",
+    "align-items-center"
+  );
+
+  todoNumber.classList.add("todo-no");
+  todoDetail.classList.add("todo-detail", "text-muted");
+  todoStatus.classList.add("todo-status", "text-muted");
+  todoActions.classList.add(
+    "todo-actions",
+    "d-flex",
+    "justify-content-start",
+    "gap-2"
+  );
+  deleteButton.classList.add("btn", "btn-danger");
+  finishedButton.classList.add("btn", "btn-success");
+
   todoNumber.textContent = "1";
   todoDetail.textContent = todoData;
   todoStatus.textContent = "In Progress";
